d44313b51705311ef86db7848497047bbc8bcd40
THEO GLINSKY <50237129@ort>
2026-06-03 08:18:00 -0300

    arrancando

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Ahorcado_danielepelbaum_theoglinsky.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
